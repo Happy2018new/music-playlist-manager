6b82b86c45ca73282127ba0d12f89ac7f696e1f5
Happy2018new <chenbo79800@163.com>
2026-05-04 17:57:52 +0800

    src: Provide comments for this project

--- src/PlaylistManager.java
+++ src/PlaylistManager.java
@@ -5,6 +5,19 @@ public class PlaylistManager {
     private Song[] playlist = new Song[MAX_SONGS];
     private int count = 0;
 
+    /**
+     * addSong adds a new song to the playlist.
+     * 
+     * If the playerlist is full, or the given song is invalid,
+     * or the given song ID already exists, then this method does
+     * nothing but output corresponding messages.
+     * 
+     * Otherwise, everything work as well, the new song successfully
+     * added, and the output message will show the details of the added song.
+     * 
+     * @param song The new song want to add.
+     * @return The output message.
+     */
     public String addSong(Song song) {
         String validationError;
 
@@ -30,6 +43,15 @@ public class PlaylistManager {
         return "Song added:\n" + song;
     }
 
+    /**
+     * deleteSongById removes a song from the playlist by given ID.
+     * 
+     * If the target song not found, then nothing will be removed.
+     * Otherwise, the target song will be removed from the playlist.
+     * 
+     * @param id The song ID of the song want to remove.
+     * @return The output message.
+     */
     public String deleteSongById(int id) {
         int indexToRemove = -1;
         Song removed = null;
@@ -54,6 +76,13 @@ public class PlaylistManager {
         return "Song deleted:\n" + removed;
     }
 
+    /**
+     * searchSongByTitle searches a song by the given song title.
+     * If multiple songs have the same title, only the first song will be returned.
+     * 
+     * @param title The given song title.
+     * @return The output message.
+     */
     public String searchSongByTitle(String title) {
         Song target = null;
 
@@ -76,8 +105,11 @@ public class PlaylistManager {
         return "Target song with title \"" + title + "\" was found, and it is:\n" + target;
     }
 
-    // Sorting feature: alphabetical by title ascending (A -> Z)
-    // No built-in sorting allowed
+    /**
+     * sortSongs sorts the songs alphabetical by title ascending (A -> Z).
+     * 
+     * @return The output message.
+     */
     public String sortSongs() {
         for (int i = 0; i < this.count; i++) {
             for (int j = i; j < this.count; j++) {
@@ -92,6 +124,11 @@ public class PlaylistManager {
         return "Playlist sorted by title.\n\n" + listAllSongs();
     }
 
+    /**
+     * listAllSongs lists all songs and put their info into the returned string.
+     * 
+     * @return The output message.
+     */
     public String listAllSongs() {
         if (count == 0) {
             return "Playlist is empty.";
--- src/Song.java
+++ src/Song.java
@@ -5,7 +5,20 @@ public class Song {
     private int duration; // seconds
     private String genre;
 
-    // Constructor
+    /**
+     * Initializes a new Song object with the following parameters.
+     * 
+     * Note that this constructor will not check the given args are correct or not,
+     * and this is your repsonsibility to calling {@link #validateSong} to check
+     * the returned Song object is valid or not.
+     * 
+     * @param id       The id of the song, must be a positive integer.
+     * @param title    The title of the song, cannot be empty.
+     * @param artist   The artist of the song, cannot be empty.
+     * @param duration The duration of the song in seconds, must be a positive
+     *                 integer.
+     * @param genre    The genre of the song, cannot be empty.
+     */
     public Song(int id, String title, String artist, int duration, String genre) {
         this.id = id;
         this.title = title;
@@ -14,6 +27,20 @@ public class Song {
         this.genre = genre;
     }
 
+    /**
+     * validateSong checks current song object is valid or not.
+     * 
+     * Note that it is your responsibility to call this method after you
+     * init or update a Song object, to check the song is valid or not.
+     * 
+     * If check failed, then validateSong returns a non-null String,
+     * which this string represents the reason why the song is invalid.
+     * 
+     * If all things work as well, meaning the song is valid,
+     * then ensure validateSong returns a null String value.
+     * 
+     * @return A non-null String if the song is invalid, otherwise null.
+     */
     public String validateSong() {
         if (this.id <= 0) {
             return "validateSong: The song ID must be a positive integer.";
@@ -33,42 +60,124 @@ public class Song {
         return null;
     }
 
+    /**
+     * getSongID returns the ID of the song.
+     * 
+     * If {@link #validateSong} is called and returns null,
+     * then make sure {@link #getSongID} returns a positive integer.
+     * 
+     * @return The ID of the song.
+     */
     public int getSongID() {
         return this.id;
     }
 
+    /**
+     * getSongTitle returns the title of the song.
+     * 
+     * If {@link #validateSong} is called and returns null,
+     * then make sure {@link #getSongTitle} returns a non-empty string.
+     * 
+     * @return The title of the song.
+     */
     public String getSongTitle() {
         return this.title;
     }
 
+    /**
+     * getSongArtist returns the artist of the song.
+     * 
+     * If {@link #validateSong} is called and returns null,
+     * then make sure {@link #getSongArtist} returns a non-null string.
+     * 
+     * A non-null string still can be an empty string,
+     * but it will never be a null string.
+     * 
+     * @return The artist of the song.
+     */
     public String getSongArtist() {
         return this.artist;
     }
 
+    /**
+     * getSongDuration returns the duration of the song in seconds.
+     *
+     * If {@link #validateSong} is called and returns null,
+     * then make sure {@link #getSongDuration} returns a positive integer.
+     * 
+     * @return The duration of the song in seconds.
+     */
     public int getSongDuration() {
         return this.duration;
     }
 
+    /**
+     * getSongGenre returns the genre of the song.
+     *
+     * If {@link #validateSong} is called and returns null,
+     * then make sure {@link #getSongGenre} returns a non-null string.
+     * 
+     * A non-null string still can be an empty string,
+     * but it will never be a null string.
+     * 
+     * @return The genre of the song.
+     */
     public String getSongGenre() {
         return this.genre;
     }
 
+    /**
+     * setSongID sets the ID of the song.
+     * 
+     * Make sure the each calling to this method will result as an exception,
+     * because the song ID cannot be changed after the Song object is created.
+     * 
+     * @param id The new ID of the song.
+     * @throws Exception The song ID cannot be changed once set, so here will always
+     *                   throw an exception.
+     */
     public void setSongID(int id) throws Exception {
         throw new Exception("setSongID: The song ID can not be changed once set.");
     }
 
+    /**
+     * setSongTitle sets the title of the song.
+     * Please make sure the title is a non-empty string.
+     * 
+     * @param title The new title of the song.
+     */
     public void setSongTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * setSongArtist sets the artist of the song.
+     * Please make sure the artist is a non-null string,
+     * but the string can still be empty.
+     * 
+     * @param artist The new artist of the song.
+     */
     public void setSongArtist(String artist) {
         this.artist = artist;
     }
 
+    /**
+     * setSongDuration sets the duration of the song in seconds.
+     * Please make sure the duration is a positive integer.
+     * 
+     * @param duration The new duration of the song in seconds.
+     */
     public void setSongDuration(int duration) {
         this.duration = duration;
     }
 
+    /**
+     * setSongGenre sets the genre of the song.
+     * Please make sure the genre is a non-null string,
+     * but the string can still be empty.
+     * 
+     * @param genre The new genre of the song.
+     */
     public void setSongGenre(String genre) {
         this.genre = genre;
     }
